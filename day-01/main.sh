86e6e227e62e3083bab1902335f0dec9cd71ac62
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-12-07 14:22:21 +0100

    refactor(d1): remove redundant xargs call

--- day-01/main.sh
+++ day-01/main.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cat input.txt | paste -sd '+' - | sed 's/++/\n/g' | xargs -I {} echo {} | bc | sort -n | tail -n 1
-cat input.txt | paste -sd '+' - | sed 's/++/\n/g' | xargs -I {} echo {} | bc | sort -n | tail -n 3 | paste -sd '+' - | bc
+cat input.txt | paste -sd '+' - | sed 's/++/\n/g' | bc | sort -n | tail -n 1
+cat input.txt | paste -sd '+' - | sed 's/++/\n/g' | bc | sort -n | tail -n 3 | paste -sd '+' - | bc
