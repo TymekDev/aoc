80f7f86d131ea0423e5de3b38a0425355dbea7a7
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-12-14 14:46:39 +0100

    feat(d10p2): add Go solution

--- day-10/main.go
+++ day-10/main.go
@@ -9,16 +9,35 @@ import (
 
 func main() {
 	aoc.RunSolution(part1, "\n")
+	aoc.RunSolution(part2, "\n")
+}
+
+func part2(input []string) (string, error) {
+	c, err := solution(input)
+	if err != nil {
+		return "", err
+	}
+
+	return c.render(), nil
 }
 
 func part1(input []string) (int, error) {
+	c, err := solution(input)
+	if err != nil {
+		return 0, err
+	}
+
+	return c.result, nil
+}
+
+func solution(input []string) (*cycler, error) {
 	c := newCycler()
 	for _, line := range input {
 		_, value, found := strings.Cut(line, " ")
 		if found {
 			x, err := strconv.Atoi(value)
 			if err != nil {
-				return 0, err
+				return nil, err
 			}
 
 			c.add(x)
@@ -27,7 +46,7 @@ func part1(input []string) (int, error) {
 		}
 	}
 
-	return c.result, nil
+	return c, nil
 }
 
 type cycler struct {
@@ -35,10 +54,15 @@ type cycler struct {
 	register int
 	result   int
 	targets  []int
+	screen   []string
 }
 
 func newCycler() *cycler {
-	return &cycler{register: 1, targets: []int{20, 60, 100, 140, 180, 220}}
+	return &cycler{
+		register: 1,
+		targets:  []int{20, 60, 100, 140, 180, 220},
+		screen:   []string{},
+	}
 }
 
 func (c *cycler) add(i int) {
@@ -48,6 +72,12 @@ func (c *cycler) add(i int) {
 }
 
 func (c *cycler) tick() {
+	if c.register-1 <= c.cycle%40 && c.cycle%40 <= c.register+1 {
+		c.screen = append(c.screen, "#")
+	} else {
+		c.screen = append(c.screen, ".")
+	}
+
 	c.cycle++
 
 	if len(c.targets) == 0 {
@@ -58,4 +88,15 @@ func (c *cycler) tick() {
 		c.result += target * c.register
 		c.targets = c.targets[1:]
 	}
+}
+
+func (c *cycler) render() string {
+	i := 0
+	var sb strings.Builder
+	for 40*(i+1) <= len(c.screen) {
+		sb.WriteString(strings.Join(c.screen[40*i:40*(i+1)], ""))
+		sb.WriteString("\n")
+		i++
+	}
+	return sb.String()
 }
